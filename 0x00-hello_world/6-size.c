1333990f80a1b8995756d57a7e9a41d2c276a749
Adebola <pelumiadeyinka69@gmail.com>
2023-02-17 00:15:51 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,6 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a char: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a char: %lu byte(s)\n", sizeof(float));
-	printf("Size of a char: %lu byte(s)\n", sizeof(double));
-	printf("Size of a char: %lu byte(s)\n", sizeof(long double));
 
 	return (0);
 }
