f1195ce0b7a02621d2fcaff00747d2234fb09c98
Adebola <pelumiadeyinka69@gmail.com>
2023-02-18 02:47:08 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = '0'; numbers <= '9'; numbers++)
+	{
+		putchar(numbers);
+		if (numbers != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
