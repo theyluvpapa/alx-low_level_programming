78fb4122fb570d8fb78da325fa4cfa72c63b9eeb
Adebola <pelumiadeyinka69@gmail.com>
2023-02-21 15:06:24 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <unistd.h>
-
+/**
+ * main - Check description
+ * Description: It prints '_putchar' followed by a newline
+ * Return: 0
+ */
 int main(void)
 {
-	const char* message = "_putchar\n";
-    write(STDOUT_FILENO, message, 9);
-    return 0;
+	const char *message = "_putchar\n";
+
+	write(STDOUT_FILENO, message, 9);
+	return (0);
 }
