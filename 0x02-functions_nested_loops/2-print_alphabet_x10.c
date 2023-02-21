850d9f269538c1a61be22801e97fc5b51f16717c
Adebola <pelumiadeyinka69@gmail.com>
2023-02-21 21:31:45 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Check main
+ * Description: _putchar function to print alphabet in lowercase 10 times
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+	}
+	_putchar('\n');
+}
