315f7feec908ed49e87cca81eb25cd0936702ba2
Adebola <pelumiadeyinka69@gmail.com>
2023-02-22 02:36:59 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - check main
+ * @a: integer input
+ * @b: integer input
+ * Description: adds two integers
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
