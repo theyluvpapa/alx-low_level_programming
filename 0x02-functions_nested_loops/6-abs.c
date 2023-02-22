f24e41c72811024b279d52ceec3c35ef06a91e1b
Adebola <pelumiadeyinka69@gmail.com>
2023-02-22 01:08:19 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Check main
+ * @int: integer input
+ * Description: Computes absolute value of integer
+ * Return: 0
+ */
+int _abs(int)
+{
+	int num;
+
+	if (num >= 0)
+	{
+		return (num);
+	}
+	else
+	{
+		return (num * -1);
+	}
+}
