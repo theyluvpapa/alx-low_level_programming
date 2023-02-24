f91fdcbe1c2e4650465f70f6fac9451f04e8a9c0
Adebola <pelumiadeyinka69@gmail.com>
2023-02-24 23:39:04 +0000

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && !(i % 5 == 0))
+		if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0 && !(i % 3 == 0))
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -26,5 +26,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+	printf('\n');
 	return (0);
 }
