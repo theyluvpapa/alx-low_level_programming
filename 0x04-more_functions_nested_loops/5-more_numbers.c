918854ea1d0ac5fcfe9755306d4f9fdd23db1e8c
Adebola <pelumiadeyinka69@gmail.com>
2023-02-24 01:28:58 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0-14 10 times
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
