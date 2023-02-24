fbe6564994248a629689161df0eca03b5af7ccb3
Adebola <pelumiadeyinka69@gmail.com>
2023-02-24 00:30:46 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: input parameter
+ * @b: input parameter
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	result = a * b;
+	return (result);
+}
