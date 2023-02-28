bebbb0bc9566d9716bd4ef26637245cd85881df6
Adebola <pelumiadeyinka69@gmail.com>
2023-02-28 01:48:07 +0000

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (*(s + len))
+	while (s[len])
 	{
 		len++;
 	}
 
 	for (int i = len - 1; i >= 0; i--)
 	{
-		_putchar(*(s + i));
+		_putchar(s[i]);
 	}
 
 	_putchar('\n');
