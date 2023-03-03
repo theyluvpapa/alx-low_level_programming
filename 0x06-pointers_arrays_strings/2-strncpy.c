d5391679fbe894e2b70d5c5675b7317316c3f2e5
Adebola <pelumiadeyinka69@gmail.com>
2023-03-03 02:38:40 +0000

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,27 +8,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int i = 0;
 
-	while (*src)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		srclen++;
-		src++;
+		dest[i] = src[i];
 	}
-
-	srclen++;
-
-	if (n > srclen)
-	{
-		n = srclen;
-	}
-
-	src = start;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src++;
+		dest[i] = '\0';
 	}
 
 	return (temp);
