e12b42239c0c0012c840d0c43ab2b0f359bb5ddb
Adebola <pelumiadeyinka69@gmail.com>
2023-03-03 03:06:28 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - function changes all lowercase letters of string to upper
+ * @s: an input string
+ * Return: char pointer to converted string
+ */
+char *string_toupper(char *s)
+{
+	char *start = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s -= 32;
+		}
+		s++
+	}
+	return (start);
+}
