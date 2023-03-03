1ee8164264dc98fe3f870f38c58bcdffad68b8a2
Adebola <pelumiadeyinka69@gmail.com>
2023-03-03 02:20:29 +0000

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  * _strncat - a function that concatenates two strings
  * @dest: an input string
  * @src: an input string
- * @n an input integer
+ * @n: an input integer
  * Return: A pointer to the resulting string
  */
 char *_strncat(char *dest, char *src, int n)
@@ -35,5 +35,5 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	*dest = '\0';
-	return (temp)
+	return (temp);
 }
